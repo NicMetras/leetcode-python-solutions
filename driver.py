27d0dbf63b04915ceef96d932d3a1191394a80eb
Nic Metras <nicolas.metras@gmail.com>
2024-08-12 14:07:04 -0700

    Add Solution to problem 2678

--- driver.py
+++ driver.py
@@ -6,5 +6,11 @@ def main():
 
     solver.hello_world()
 
+    # Testing solution to problem 2678. Number of Senior Citizens
+    details = ["7868190130M7522", "5303914400F9211", "9273338290F4010"]
+
+    result = solver.countSeniors(details)
+    print(f"Solution to problem 2678: {result}")
+
 if __name__ == "__main__":
     main()
--- solution.py
+++ solution.py
@@ -4,3 +4,14 @@ class Solution:
 
     def hello_world(self):
         print(f'Hi, World!')
+
+    # Solution to 2678 Number of Senior Citizens
+    def countSeniors(self, details) -> int:
+        count = 0
+        for record in details:
+            if int(record[11:13]) > 60:
+                count += 1
+        return count
+
+
+
