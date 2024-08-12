21c9bfb48e48506be88763cc4ad62f8ce88dd485
Nic Metras <nicolas.metras@gmail.com>
2024-08-12 10:20:55 -0700

    Add Solution class and driver

---
+++ driver.py
@@ -0,0 +1,10 @@
+from solution import Solution
+
+def main():
+    # Create an instance of the Solution class
+    solver = Solution()
+
+    solver.hello_world()
+
+if __name__ == "__main__":
+    main()
---
+++ solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def __init__(self):
+        pass
+
+    def hello_world(self):
+        print(f'Hi, World!')
